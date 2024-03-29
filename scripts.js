773dffa297b7187e5bb07103e3dbf771c79c24ef
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-03-29 23:34:02 +0200

    Added scrolling behavior change on the bottom of navigation bar.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
+// Codes for adding a box shadow on the bottom of the navigation when scrolling.
+
 const navigation = document.querySelector('.navigation');
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 0) {
-        return navigation.classList.add("scrolling-effect")
-    } else {
-        return navigation.classList.remove("scrolling-effect")
-    }
+    if(window.scrollY > 0)
+        return navigation.classList.add("scrolling-effect");
+    else 
+        return navigation.classList.remove("scrolling-effect");
 })
